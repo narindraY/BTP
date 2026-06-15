24fdb0ca7c210b9dda62a7030ecd623fc4b87630
onjampitiavana <santatriniaina829@gmail.com>
2026-06-15 03:06:11 +0300

    conflit chat and features

--- backend/src/config/db.nante.js
+++ backend/src/config/db.nante.js
@@ -2,10 +2,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+    host: "localhost",
+    user: "root",
+    database: "btp",
+    password: ""
 });
 
 
